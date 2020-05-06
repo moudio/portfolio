9b2be85909e8836623a5c7e2ad3fc1156147c7f9
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-05-06 11:01:10 +0000

    Updates

---
+++ precache-manifest.11da447f2c30a6cbb3a283c7325b128a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e4ef9b9ddae714dc7838e79a518187",
+    "url": "//mouhadiouf.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "e562643505a842c01dc4",
+    "url": "//mouhadiouf.github.io/portfolio/static/css/main.df2da710.chunk.css"
+  },
+  {
+    "revision": "d4c641824b1230f62052",
+    "url": "//mouhadiouf.github.io/portfolio/static/js/2.f56c261b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//mouhadiouf.github.io/portfolio/static/js/2.f56c261b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e562643505a842c01dc4",
+    "url": "//mouhadiouf.github.io/portfolio/static/js/main.a0a314b1.chunk.js"
+  },
+  {
+    "revision": "0d198e493301d67b5c82",
+    "url": "//mouhadiouf.github.io/portfolio/static/js/runtime-main.be5266cb.js"
+  },
+  {
+    "revision": "77c55f9e57751b1f1458c2de645a8437",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/Battleship.77c55f9e.jpg"
+  },
+  {
+    "revision": "1d48a301e10fa8907a62bfd56b97c19d",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/Blackjack.1d48a301.jpg"
+  },
+  {
+    "revision": "85e7011b6f296de2d452e45df70206d6",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/Bookstore.85e7011b.png"
+  },
+  {
+    "revision": "c0e040bfecb18b4c7990e20c18839f8a",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/Calculator.c0e040bf.jpg"
+  },
+  {
+    "revision": "e186e06c235c458e251675ac16d7ea9a",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/Finance.e186e06c.jpg"
+  },
+  {
+    "revision": "e2b64c2288c02ad0e0a109b3a8812c78",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/Header.e2b64c22.jpg"
+  },
+  {
+    "revision": "05a70721f942a0dc224d6b8d5eab060a",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/Newsweek.05a70721.jpg"
+  },
+  {
+    "revision": "a23d1fffca71af12be0c9d8280ff065a",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/Restaurant.a23d1fff.png"
+  },
+  {
+    "revision": "1b85e87000d6851a31de7a92b8be1394",
+    "url": "//mouhadiouf.github.io/portfolio/static/media/profile.1b85e870.jpg"
+  }
+]);

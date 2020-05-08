94170cdadffbe66c5352fe13164a71f535ffe5cc
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-05-08 10:25:41 +0000

    Updates

---
+++ precache-manifest.403981f68cbd513835ec21f2e8e0ff56.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20448f14d549ecb39881f0592fbd7d84",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "67d392f896fbbe67274d",
+    "url": "/portfolio/static/css/main.50f8b8c7.chunk.css"
+  },
+  {
+    "revision": "d4c641824b1230f62052",
+    "url": "/portfolio/static/js/2.f56c261b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f56c261b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d392f896fbbe67274d",
+    "url": "/portfolio/static/js/main.a0a314b1.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "77c55f9e57751b1f1458c2de645a8437",
+    "url": "/portfolio/static/media/Battleship.77c55f9e.jpg"
+  },
+  {
+    "revision": "1d48a301e10fa8907a62bfd56b97c19d",
+    "url": "/portfolio/static/media/Blackjack.1d48a301.jpg"
+  },
+  {
+    "revision": "85e7011b6f296de2d452e45df70206d6",
+    "url": "/portfolio/static/media/Bookstore.85e7011b.png"
+  },
+  {
+    "revision": "c0e040bfecb18b4c7990e20c18839f8a",
+    "url": "/portfolio/static/media/Calculator.c0e040bf.jpg"
+  },
+  {
+    "revision": "e186e06c235c458e251675ac16d7ea9a",
+    "url": "/portfolio/static/media/Finance.e186e06c.jpg"
+  },
+  {
+    "revision": "e2b64c2288c02ad0e0a109b3a8812c78",
+    "url": "/portfolio/static/media/Header.e2b64c22.jpg"
+  },
+  {
+    "revision": "05a70721f942a0dc224d6b8d5eab060a",
+    "url": "/portfolio/static/media/Newsweek.05a70721.jpg"
+  },
+  {
+    "revision": "a23d1fffca71af12be0c9d8280ff065a",
+    "url": "/portfolio/static/media/Restaurant.a23d1fff.png"
+  },
+  {
+    "revision": "1b85e87000d6851a31de7a92b8be1394",
+    "url": "/portfolio/static/media/profile.1b85e870.jpg"
+  }
+]);

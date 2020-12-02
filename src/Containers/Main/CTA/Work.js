8c640a6a7302935f28cfd22db1d919bf14c6ec97
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-02 11:18:13 +0000

    Update the Call to action component

--- src/Containers/Main/CTA/Work.js
+++ src/Containers/Main/CTA/Work.js
@@ -7,9 +7,8 @@ function Work() {
       <h2>Let&apos;s Work Together</h2>
       <div id="work-together">
         <p>
-          Have a project, are you thinking about building your website or
-          creating a web application?
-          {' '}
+          Are you thinking about building your website or creating a web
+          application?{' '}
           <a className="contact" href="mailto: mouhamadoudioufsn@gmail.com">
             Contact me!
           </a>
